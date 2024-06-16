eeda5b2c36a8fcf771e282f72d0fbac7e63b4468
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 19:30:27 +0300

    Initial commit

---
+++ SummerOutfit/Program.cs
@@ -0,0 +1,82 @@
+﻿class SummerOutfit
+{
+    public static void Main(String[] args)
+    {
+        int temperature = int.Parse(Console.ReadLine());
+        string timeOfDay = Console.ReadLine();
+
+        if (timeOfDay == "Morning")
+        {
+            if (temperature >= 10 && temperature <= 18)
+            {
+                string clothing = "Sweatshirt";
+                string shoes = "Sneakers";
+                Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+            }
+            else if (temperature > 18 && temperature <= 24)
+            {
+
+                string clothing = "Shirt";
+                string shoes = "Moccasins";
+                Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+            }
+            else if (temperature >= 25)
+            {
+
+                string clothing = "T-Shirt";
+                string shoes = "Sandals";
+                Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+
+            }
+        }
+
+        if (timeOfDay == "Afternoon")
+        {
+            if (temperature >= 10 && temperature <= 18)
+            {
+                string clothing = "Shirt";
+                string shoes = "Moccasins";
+                Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+            }
+            else if (temperature > 18 && temperature <= 24)
+            {
+
+                string clothing = "T-Shirt";
+                string shoes = "Sandals";
+                Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+            }
+            else if (temperature >= 25)
+            {
+
+                string clothing = "Swim Suit";
+                string shoes = "Barefoot";
+                Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+            }
+        }
+
+        if (timeOfDay == "Evening")
+        {
+            if (temperature >= 10 && temperature <= 18)
+            {
+                string clothing = "Shirt";
+                string shoes = "Moccasins";
+                Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+            }
+            else if (temperature > 18 && temperature <= 24)
+            {
+
+                string clothing = "Shirt";
+                string shoes = "Moccasins";
+                Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+            }
+            else if (temperature >= 25)
+            {
+
+                string clothing = "Shirt";
+                string shoes = "Moccasins";
+                Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+            }
+        }
+        
+    }
+}
